35833fbfe9db25998a693cb692ba982ef21b9892
SashaLogov <partyhardteam@gmail.com>
2025-05-23 18:09:46 -0700

    com

--- src/react-app/App.jsx
+++ src/react-app/App.jsx
@@ -36,6 +36,13 @@ export const App = () => (
   <div className="container">
     <div className="nav">
       <h1 className="label" />
+      <div />
+      <span className="caption" />
+      <div />
+      <a href="https://l69l.com">
+        <div className="button">SWOREEL</div>
+      </a>
+      <div className="button gray">///</div>
     </div>
     <Scene style={{ borderRadius: 20 }} />
   </div>
